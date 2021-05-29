fd81753a3b75f1b9e228b6bc8e73d0f8c27a8ad0
Milo Fultz <milopfultz@gmail.com>
2021-05-29 16:00:46 -0700

    Walk through files

--- swiki.py
+++ swiki.py
@@ -15,20 +15,35 @@ pages dict schema
 
 """
 
-# import marko
+import os
+import sys
 
-# create pages dict
+import marko
 
-# for each file in all subfolders
-    # get front matter and markdown from file
-    # get all local links in the file and add to dict "links" prop
-    # for each link
-        # if page not in dict, make one
-        # add current page to "backlinks" prop
-    # add a local link to any {{...}} words (href="lower-kebab-case-title.html")
-    # split relative path into folders and set "folder" to that or just ['.']
+def make_wiki(dir: str):
+    # create pages dict
+    pages = dict()
 
-# for each page in dict
-    # render the markdown
-    # fill HTML `head` with front matter
-    # put into HTML frame (has everything but the content)
+    # for each file in all subfolders
+    for subfolder, _, files in os.walk(dir):
+        for file in files:
+            with open(os.path.join(subfolder, file), 'r') as f:
+                print(file)
+                # get front matter and markdown from file
+                # get all local links in the file and add to dict "links" prop
+                # for each link
+                    # if page not in dict, make one
+                    # add current page to "backlinks" prop
+                # add a local link to any {{...}} words (href="lower-kebab-case-title.html")
+                # split relative path into folders and set "folder" to that or just ['.']
+
+    # for each page in dict
+        # render the markdown
+        # fill HTML `head` with front matter
+        # put into HTML frame (has everything but the content)
+
+
+if __name__ == "__main__":
+    dir = sys.argv[1] if len(sys.argv) == 2 else os.getcwdb()
+
+    make_wiki(dir)
