4cf69db65884e5c2caebe442df0d3890a3f4271b
Milo Fultz <milopfultz@gmail.com>
2021-05-29 16:48:27 -0700

    Render internal links and basic page

--- swiki.py
+++ swiki.py
@@ -15,6 +15,7 @@ pages dict schema
 
 """
 
+from collections import defaultdict
 import os
 import re
 import sys
@@ -22,34 +23,65 @@ import sys
 import marko
 import frontmatter
 
-re_wikilink = re.compile(r'(?<={{).+?(?=}})')
+re_wikilink = re.compile(r'{{.+?}}')
+
+
+def kebabify(text: str) -> str:
+    return text.replace(' ', '-').lower()
+
+
+def add_local_links(html: str) -> str:
+    def make_link(match: re.Match):
+        text = match.group()[2:-2]
+        filename = kebabify(text)
+        return f'<a href="{filename}.html">{text}</a>'
+    return re_wikilink.sub(make_link, html)
 
 
 def make_wiki(pages_dir: str):
     # create pages dict
-    pages = dict()
+    pages = defaultdict(dict)
 
     # for each page in pages
     for subfolder, _, files in os.walk(pages_dir):
         for file in files:
+            page = dict()
+            page_filename = kebabify(os.path.splitext(file)[0])
+            # split relative path into folders and set "folder" to that or just ['.']
+            page['folder'] = os.path.split(subfolder)
             with open(os.path.join(subfolder, file), 'r') as f:
                 file_contents = f.read()
                 # get front matter and markdown from file
-                metadata, content = frontmatter.parse(file_contents)
+                page['metadata'], page['content'] = frontmatter.parse(file_contents)
             # get all local links in the file and add to dict "links" prop
-            links = re_wikilink.findall(content)
-            print(links)
+            page['links'] = re_wikilink.findall(page.get('content'))
 
             # for each link
+            for link in page['links']:
+                # Remove the curlys and kebabify
+                link_filename = kebabify(link[2:-2])
                 # if page not in dict, make one
+                if not pages[link_filename].get('backlinks'):
+                    pages[link_filename]['backlinks'] = []
                 # add current page to "backlinks" prop
-            # add a local link to any {{...}} words (href="lower-kebab-case-title.html")
-            # split relative path into folders and set "folder" to that or just ['.']
+                pages[link_filename]['backlinks'].append(page_filename)
+
+            # add page info to pages dict
+            if pages.get(page_filename):
+                pages[page_filename] |= page
+            else:
+                pages[page_filename] = page
+
 
     # for each page in dict
+    for page, info in pages.items():
         # render the markdown
+        html = marko.convert(info.get('content', ''))
+        # add a local link to any {{...}} words (href="lower-kebab-case-title.html")
+        html_with_links = add_local_links(html)
         # fill HTML `head` with front matter
         # put into HTML frame (has everything but the content)
+        print(html_with_links)
 
 
 if __name__ == "__main__":
