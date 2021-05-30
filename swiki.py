e20c7b40e8880a9e7f8228ef4b8f55b55ec6ccb6
Milo Fultz <milopfultz@gmail.com>
2021-05-29 18:34:33 -0700

    Revise docs

--- swiki.py
+++ swiki.py
@@ -1,73 +1,43 @@
-"""
-pages dict schema
-{
-    page_name: {
-        "metadata": {
-            'title': str,
-            "description": str
-        },
-        "content": str,
-        "links": list[str],
-        "backlinks": list[(str, str)]
-    },
-    ...
-}
-
-sitemap dict schema
-{
-    folder_name str: [
-        {
-            'title': str,
-            'filename': str
-        },
-        ...
-    ],
-    ...
-}
-"""
-
 from collections import defaultdict
 import os
-from pprint import pprint
 import re
 import sys
 
 import marko
 import frontmatter
 
+
+IGNORE = ['.DS_Store']
 re_wikilink = re.compile(r'{{.+?}}')
 
 
 def make_page_dict(subfolder: str, file: str, rel_path: str) -> dict:
+    """ Make dict of all page specific data """
     page = dict()
-    # split relative path into folders and set "folder" to that or just ['.']
     page['folder'] = rel_path
     with open(os.path.join(subfolder, file), 'r') as f:
         file_contents = f.read()
-        # get front matter and markdown from file
         page['metadata'], page['content'] = frontmatter.parse(file_contents)
-    # get all local links in the file and add to dict "links" prop
+    # get all local links in the file
     page['links'] = re_wikilink.findall(page.get('content'))
     return page
 
 
 def add_page_to_sitemap(page_info: dict, folder: str, sitemap: defaultdict):
-    # sitemap_page = {title: title, filename: filename}
-    # get folder list from sitemap or [] if nothing
+    """ Add page info to sitemap """
     updated_folder = sitemap.get(folder, [])
-    # add to folder list
     updated_folder.append(page_info)
-    # replace folder list in sitemap
     sitemap[folder] = updated_folder
-    # return sitemap
     return sitemap
 
 
 def kebabify(text: str) -> str:
+    """ Format text to filename kebab-case """
     return text.replace(' ', '-').lower()
 
 
 def add_local_links(html: str) -> str:
+    """ Replace all {{...}} with anchor tags """
     def make_link(match: re.Match):
         text = match.group()[2:-2]
         filename = kebabify(text)
@@ -75,37 +45,25 @@ def add_local_links(html: str) -> str:
     return re_wikilink.sub(make_link, html)
 
 
-def add_title(content: str, title: str) -> str:
-    return f'<h1>{title}</h1>\n{content}'
-
-
-def contain(element: str, html_id: str, content: str) -> str:
+def place_in_container(element: str, html_id: str, content: str) -> str:
+    """ Place content in container with ID """
     return f'<{element} id="{html_id}">{content}</{element}>'
 
 
 def add_backlinks(content: str, backlinks: list) -> str:
-    # if no backlinks
+    """ Add backlinks section to content """
     if not backlinks:
-        # return content
         return content
-    # create div
-    backlinks_html = '<section id="backlinks">'
-    # create heading and unordered list
-    backlinks_html += '<h2>Backlinks:</h2><ul>'
-    # for each link
+    backlinks_html = '<section id="backlinks"><h2>Backlinks:</h2><ul>'
     for backlink in backlinks:
         title, filename = backlink.get('title'), backlink.get('filename')
-        # fill out list item and link inside
         backlinks_html += f'<li><a href="{filename}.html">{title}</a></li>'
-    # close unordered list
-    backlinks_html += '</ul>'
-    # close div
-    backlinks_html += '</section>'
-    # append new div to content
+    backlinks_html += '</ul></section>'
     return content + backlinks_html
 
 
 def fill_frame(frame: str, content: str, metadata: dict) -> str:
+    """ Fill out HTML frame with page information """
     frame = frame.replace('{{title}}', metadata.get('title'))
     frame = frame.replace('{{description}}', metadata.get('description'))
     frame = frame.replace('{{content}}', content)
@@ -113,38 +71,30 @@ def fill_frame(frame: str, content: str, metadata: dict) -> str:
 
 
 def make_sitemap(sitemap: dict, output_dir: str):
+    """ Make sitemap out of all seen pages """
     sitemap_html = ''
-    # for each folder
     for folder, folder_list in sitemap.items():
-        # make a header of the folder name
-        sitemap_html += f'<h2>{folder or "[root]"}</h2>'
-        # make an unordered list
-        sitemap_html += '<ul>'
+        sitemap_html += f'<h2>{folder or "[root]"}</h2><ul>'
         # sort the contents on the folder list by title
         sorted(folder_list, key=lambda page: page.get('title'))
-        # for each file in folder
         for page in folder_list:
             title, filename = page.get('title'), page.get('filename')
-            # make list and link for file
             sitemap_html += f'<li><a href="{filename}.html">{title}</a></li>'
-        # close ul
         sitemap_html += '</ul>'
-    # contain html
-    sitemap_html = contain('main', 'sitemap', sitemap_html)
-    # write to _sitemap.html
+    sitemap_html = place_in_container('main', 'sitemap', sitemap_html)
     with open(os.path.join(output_dir, '_sitemap.html'), 'w') as f:
         f.write(sitemap_html)
 
 
 def make_wiki(pages_dir: str, output_dir: str):
-    # create pages dict
+    """ Create flat wiki out of all pages """
     pages = defaultdict(dict)
     sitemap = defaultdict(dict)
 
-    # for each page in pages
     for subfolder, _, files in os.walk(pages_dir):
         for file in files:
-            if file[0:2] == '__':
+            # if wiki meta file or not a markdown page, ignore it
+            if file[0:2] == '__' or os.path.splitext(file)[1] != '.md':
                 continue
             rel_path = subfolder.replace(pages_dir, '')
             page = make_page_dict(subfolder, file, rel_path)
@@ -152,9 +102,8 @@ def make_wiki(pages_dir: str, output_dir: str):
 
             # add backlinks to all pages this page links to
             for link in page['links']:
-                # Remove the curlys and kebabify
                 link_filename = kebabify(link[2:-2])
-                # if page not in dict, make one
+                # if page not yet in pages or doesn't have a page yet, make an entry
                 if not pages[link_filename].get('backlinks'):
                     pages[link_filename]['backlinks'] = []
                 # add current page to "backlinks" prop
@@ -167,33 +116,28 @@ def make_wiki(pages_dir: str, output_dir: str):
             else:
                 pages[page_filename] = page
 
+    # Load HTML frame file
     with open(os.path.join(pages_dir, '__frame.html'), 'r') as f:
         frame = f.read()
 
-    # for each page in dict
     for page, info in pages.items():
+        # If page is linked to but it hasn't been made yet, give it placeholder metadata
         if not info.get('metadata'):
             info['metadata'] = {'title': page, 'description': ''}
+            content = 'There\'s currently nothing here.'
+        else:
+            content = marko.convert(info.get('content', ''))
+            # add a local link to any {{...}} words (href="lower-kebab-case-title.html")
+            content = add_local_links(content)
         sitemap = add_page_to_sitemap({'title': info['metadata'].get('title'), 'filename': page},
                                       info.get('folder', ''),
                                       sitemap)
-        # render the markdown
-        if markdown := info.get('content', ''):
-            content = marko.convert(markdown)
-            # add a local link to any {{...}} words (href="lower-kebab-case-title.html")
-            content = add_local_links(content)
-        else:
-            content = 'There\'s currently nothing here.'
-        content = add_title(content, info['metadata'].get('title'))
-        # put content into section container
-        content = contain('section', 'content', content)
-        # add backlinks
+        content = f'<h1>{info["metadata"].get("title")}</h1>\n{content}'
+        content = place_in_container('section', 'content', content)
         content = add_backlinks(content, info.get('backlinks', []))
-        # put everything inside of a main element
-        content = contain('main', 'main', content)
-        # fill HTML `head` with front matter
-        # put into HTML frame (has everything but the content)
+        content = place_in_container('main', 'main', content)
         filled_frame = fill_frame(frame, content, info.get('metadata', []))
+
         with open(os.path.join(output_dir, f'{page}.html'), 'w') as f:
             f.write(filled_frame)
 
