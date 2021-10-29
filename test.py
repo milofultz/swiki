f7c02533987430803dff1ad1a541a61ff21c757d
Milo Fultz <milopfultz@gmail.com>
2021-10-28 17:48:45 -0700

    Add recent list length test

--- test.py
+++ test.py
@@ -872,9 +872,23 @@ class MakeWikiTestCase(unittest.TestCase):
             actual_index_file_content = f.read()
         self.assertEqual(expected_index_file_content, actual_index_file_content)
 
-    @unittest.skip("stub")
     def test_recent_list_length(self):
-        pass
+        test_recent_config = {'tab_size': 2, 'build_fatfile': False,
+                              'recent_list': True, 'recent_list_length': 1}
+        swiki.make_wiki(self.test_input_folder, self.test_output_folder,
+                        test_recent_config)
+        # should show both pages added in order of creation
+        output_index_file_path = os.path.join(self.test_output_folder, 'index.html')
+        self.assertTrue(os.path.isfile(output_index_file_path))
+        expected_index_file_content = f"""\
+<html><head><title></title><meta name="description" content=""></head><body>\
+<main id="main"><h1 id="title">Sitemap</h1><section class="recent-list"><h2>Recent Changes:</h2><ul>\
+<li>{self.another_test_file_lm}: <a href="another-file.html">Another File</a></li></ul></section>\
+<div><details><summary>[root]</summary><ul><li><a href="another-file.html">Another File</a></li>\
+<li><a href="example-file.html">Example File</a></li></ul></details></div></main></body></html>"""
+        with open(output_index_file_path, 'r') as f:
+            actual_index_file_content = f.read()
+        self.assertEqual(expected_index_file_content, actual_index_file_content)
 
     @classmethod
     def tearDownClass(cls):
